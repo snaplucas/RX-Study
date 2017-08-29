d2d766a8d34aad0a53863a1864710696fd32422f
snaplucas <lucas.chapola@gmail.com>
2017-08-28 23:08:35 -0300

    adding example using java 8

--- src/main/java/rx/study/java/Main.java
+++ src/main/java/rx/study/java/Main.java
@@ -3,10 +3,16 @@ package rx.study.java;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 
 public class Main {
     public static void main(String[] args) {
-        Observable<String> myObservable = Observable.create(
+        Observable<String> myObservable = Observable.create(sub -> {
+            sub.onNext("Hello, world!");
+            sub.onCompleted();
+        });
+
+        Observable<String> myObservable2 = Observable.create(
                 new Observable.OnSubscribe<String>() {
                     @Override
                     public void call(Subscriber<? super String> sub) {
@@ -16,18 +22,23 @@ public class Main {
                 }
         );
 
+        Subscription subscribe = Observable.just("Hello, world!").subscribe(System.out::println);
+
         Subscriber<String> mySubscriber = new Subscriber<String>() {
             @Override
-            public void onNext(String s) { System.out.println(s); }
+            public void onNext(String s) {
+                System.out.println(s);
+            }
 
             @Override
-            public void onCompleted() { }
+            public void onCompleted() {
+            }
 
             @Override
-            public void onError(Throwable e) { }
+            public void onError(Throwable e) {
+            }
         };
 
         myObservable.subscribe(mySubscriber);
-
     }
 }
